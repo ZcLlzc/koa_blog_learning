593617512f398a66076a6f589ba2b25ca47e405a
zcl <2296992449@qq.com>
2023-07-21 01:06:36 +0800

    feat：前端拆分路由 💕

--- koa_frontend/src/router/index.ts
+++ koa_frontend/src/router/index.ts
@@ -2,62 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import 'nprogress/nprogress.css'
 import NProgress from 'nprogress'
 import { getUser } from '@/utils/auth'
-
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'layout',
-    redirect: {
-      path: '/home'
-    },
-    component: () => import('@/layout/index.vue'),
-    children: [
-      {
-        path: '/home',
-        name: 'home',
-        component: () => import('@/views/home.vue'),
-        meta: {
-          requireAuth: false
-        }
-      },
-      {
-        path: '/psychology',
-        name: 'psychology',
-        component: () => import('@/views/psychology.vue'),
-        meta: {
-          requireAuth: false
-        }
-      },
-      {
-        path: '/fiction',
-        name: 'fiction',
-        component: () => import('@/views/noval/fiction.vue'),
-        meta: {
-          requireAuth: false
-        }
-      },
-      {
-        path: '/fictionDetail',
-        name: 'fictionDetail',
-        component: () => import('@/views/noval/fictionDetail.vue'),
-        meta: {
-          requireAuth: false,
-          parentMenu: 'fiction'
-        }
-      }
-    ]
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/login.vue')
-  },
-  {
-    path: '/:pathMatch(.*)',
-    name: 'error',
-    component: () => import('@/views/error.vue')
-  }
-]
+import routes from './routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_PUBLIC_PATH),
---
+++ koa_frontend/src/router/routes.ts
@@ -0,0 +1,60 @@
+import { RouteRecordRaw } from 'vue-router'
+
+// 定义路由规则
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'layout',
+    redirect: {
+      path: '/home'
+    },
+    component: () => import('@/layout/index.vue'),
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import('@/views/home.vue'),
+        meta: {
+          requireAuth: false
+        }
+      },
+      {
+        path: '/psychology',
+        name: 'psychology',
+        component: () => import('@/views/psychology.vue'),
+        meta: {
+          requireAuth: false
+        }
+      },
+      {
+        path: '/fiction',
+        name: 'fiction',
+        component: () => import('@/views/noval/fiction.vue'),
+        meta: {
+          requireAuth: false
+        }
+      },
+      {
+        path: '/fictionDetail',
+        name: 'fictionDetail',
+        component: () => import('@/views/noval/fictionDetail.vue'),
+        meta: {
+          requireAuth: false,
+          parentMenu: 'fiction'
+        }
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login.vue')
+  },
+  {
+    path: '/:pathMatch(.*)',
+    name: 'error',
+    component: () => import('@/views/error.vue')
+  }
+]
+
+export default routes
